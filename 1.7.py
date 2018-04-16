4ed99d4a78004e8826c44259bf3a2adcbe30d3ff
gutucristian <gutucristian27@gmail.com>
2018-04-16 14:52:16 -0400

    improved variable naming and added comments

--- 1.7.py
+++ 1.7.py
@@ -1,29 +1,43 @@
 def rotate_matrix(matrix):
-	'''rotates a matrix 90 degrees clockwise'''
-	n = len(matrix)
-	for layer in range(n // 2):
-		first, last = layer, n - layer - 1
+	length = len(matrix)
+	amt_of_layers = length // 2
+	for layer in range(amt_of_layers):
+		first, last = layer, length - layer - 1		
 		for i in range(first, last):
-			# save top
-			top = matrix[layer][i]
+			# Select the 4 coordinates to be rotated by 90 degrees. The manner in which coordinates are selected is governed by the 'layer' we are currently on and by the current index 'i' which, itself, is bound by the length/size of the current layer.
+			p1 = matrix[layer][i]
+			p2 = matrix[i][-layer - 1]
+			p3 = matrix[-layer - 1][-i - 1]
+			p4 = matrix[-i - 1][layer]			
 
-			# left -> top
-			matrix[layer][i] = matrix[-i - 1][layer]
+			# swap values of 4 coordinates (p1, p2, p3, p4 = p4, p1, p2, p3)
+			matrix[layer][i] = p4
+			matrix[i][-layer - 1] = p1
+			matrix[-layer - 1][-i - 1] = p2
+			matrix[-i - 1][layer] = p3
 
-			# bottom -> left
-			matrix[-i - 1][layer] = matrix[-layer - 1][-i - 1]
-
-			# right -> bottom
-			matrix[-layer - 1][-i - 1] = matrix[i][- layer - 1]
-
-			# top -> right
-			matrix[i][- layer - 1] = top
 	return matrix
 
-m = [
-  [3, 2, 1],
-  [0, 7, 6],
-  [5, 2, 4]
+def print_matrix(matrix):
+	for row in range(len(matrix)):
+		print(matrix[row])
+
+matrix = [
+  [3, 2, 1, 4],
+  [0, 7, 6, 9],
+  [5, 2, 4, 3],
+	[6, 9, 2, 4]
 ]
 
-print(rotate_matrix(m))
+print('Original:')
+print_matrix(matrix)
+print('Rotated:')
+m = rotate_matrix(matrix)
+print_matrix(matrix)
+
+# Another way to rotate matrix:
+# 1. transpose matrix (flip each elements row with column)
+# 2. change rows to rotate left or columns to rotate right
+# 'change rows' means 'flip row 0 with row n, row 1 with row n-1, etc'. Similar for columns.
+
+# Above method is easier but requires O(n) extra space.
