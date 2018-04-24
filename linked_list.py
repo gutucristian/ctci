67d7c8208029ce112edfc0afa4d0d9d6ce3adfb0
gutucristian <gutucristian27@gmail.com>
2018-04-24 14:22:29 -0400

    implement linked_list

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Node():
   def __init__(self, value, next = None, previous = None):
     self.value = value
@@ -18,7 +20,7 @@ class LinkedList:
       current = current.next
 
   def __str__(self):
-    values = [str(x) for x in self]
+    values = [str(node.value) for node in self]
     return ' -> '.join(values)
 
   def __len__(self):
@@ -28,12 +30,35 @@ class LinkedList:
       length += 1
       current = current.next
     return length
-
-  # add to end
+  
   def add(self, value):
+    """Creates Node with value and adds it to the end of the linked list.
+    
+    Keyword arguments:
+    value -- value of the node to be added
+    """
     if self.head is None:
       self.tail = self.head = Node(value)
     else:
       self.tail.next = Node(value)
       self.tail = self.tail.next
     return self.tail    
+
+  def add_to_beggining(self, value):
+    if self.head is None:
+      self.tail = self.head = Node(value)
+    else:
+      new_head = Node(value, self.head)      
+      self.head = new_head
+    return self.head
+
+  def add_multiple(self, values):
+    for value in values:
+      self.add(value)
+
+  def generate(self, n, min_value, max_value):
+    """Generates a new linked list with n nodes where each node value is in range [min_value, max_value]."""
+    self.tail = self.head = None
+    for _ in range(n):
+      self.add(randint(min_value, max_value))
+    return self
