3d092fc9f81f6238968a35c11dc7d6388c4e3cc9
gutucristian <gutucristian27@gmail.com>
2018-04-27 09:04:14 -0400

    add python inline swap

--- 2.4.py
+++ 2.4.py
@@ -7,29 +7,20 @@ def partition(ll, ref_node):
   # find ref_node and swap with first node
   for node in ll:
     if node.value == ref_node.value:
-      tmp = ll.head.value
-      ll.head.value = node.value
-      node.value = tmp
+      ll.head.value, node.value = node.value, ll.head.value
       break
   
   runner = ll.head.next
   border = ll.head
 
   while runner is not None:
-    if runner.value < ll.head.value:
-      tmp = border.next.value
-      border.next.value = runner.value
-      runner.value = tmp
+    if runner.value < ll.head.value:      
+      border.next.value, runner.value = runner.value, border.next.value
       border = border.next    
     runner = runner.next    
-
-  tmp = border.value
-  border.value = ll.head.value
-  ll.head.value = tmp
-
-  # 8, 5, 2, 13, 27, 45, 25
-  # 
   
+  border.value, ll.head.value = ll.head.value, border.value
+ 
 
 print(ll)
 partition(ll, node)
