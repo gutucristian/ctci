c39a5b8313b162e3c6f28532320d5bd60e268d06
gutucristian <gutucristian27@gmail.com>
2018-05-05 15:08:39 -0400

    solve followup question

--- ch_2/2.5.py
+++ ch_2/2.5.py
@@ -1,12 +1,10 @@
-class Node:
-	def __init__(self, value=None, next=None):
-		self.value = value
-		self.next = next
+from linked_list import LinkedList
 
-# node_1 and node_2 are the heads of two separate linked lists
-def sum_lists(node_1, node_2):
+def sum_lists_1(ll_a, ll_b):
 	carry = 0
-	head = tail = None
+	node_1, node_2 = ll_a.head, ll_b.head
+	ll = LinkedList()
+
 	while node_1 or node_2:
 		sum = carry
 		if node_1:
@@ -16,34 +14,47 @@ def sum_lists(node_1, node_2):
 			sum += node_2.value
 			node_2 = node_2.next
 
-		rem = sum % 10
 		carry = sum // 10  # gives how many times 10 fits into sum
+		ll.add(sum % 10) # adds to the end (i.e. tail) of the linked list to maintain reversed order
 
-		if head:
-			tail.next = Node(rem)
-			tail = tail.next
-		else:
-			head = tail = Node(rem)
+	if carry: ll.add(carry)
 
-	if carry: tail.next = Node(carry)
+	return ll
 
-	return head
+def sum_lists_2(node_1, node_2, carry):
+	if node_1 == None and node_2 == None and carry == None:
+		return None
+	sum = carry
+	if node_1: sum += node_1.value
+	if node_2: sum += node_2.value
+	result = Node(sum % 10)
+	if node_1 and node_2:
+		more = sum_lists_2(node_1.next, node_2.next, sum // 10)
+		result.next = more
+	return result
 
-n6 = Node(1)
-n5 = Node(0, n6)
-n4 = Node(0, n5)
+def sum_lists_followup(ll_a, ll_b):
+	# pad shorter list with zeros
+	if len(ll_a) > len(ll_b):
+		for _ in range(len(ll_a) - len(ll_b)):
+			ll_b.add_to_beginning(0)
+	else:
+		for _ in range(len(ll_b) - len(ll_a)):
+			ll_a.add_to_beginning(0)
 
-n2 = Node(9)
-n1 = Node(9, n2)
+	result = 0
+	n1, n2 = ll_a.head, ll_b.head
+	while n1 and n2:
+		result = (result * 10) + n1.value + n2.value
+		n1, n2 = n1.next, n2.next
 
-res = []
-node = sum_lists(n1, n4)
+	ll = LinkedList()
+	ll.add_multiple([int(i) for i in str(result)])
 
-while node is not None:
-  if node.next != None:
-    res.append(str(node.value) + ' -> ')
-  else:
-    res.append(str(node.value))
-  node = node.next
+	return ll
 
-print(''.join(res))
+# initial values are given in reverse because LinkedList (by default) adds values to tail
+ll_a = LinkedList([1, 0, 0])
+ll_b = LinkedList([9, 9])
+
+print(sum_lists_followup(ll_a, ll_b))
