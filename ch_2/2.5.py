4fcfb25ba93c0936c19ebbc76c5233862b22b5c8
gutucristian <gutucristian27@gmail.com>
2018-05-04 20:23:35 -0400

    solve 2.4 without using ll class

--- ch_2/2.5.py
+++ ch_2/2.5.py
@@ -5,30 +5,36 @@ class Node:
 
 # node_1 and node_2 are the heads of two separate linked lists
 def sum_lists(node_1, node_2):
-	num_1 = ll_to_num(node_1)
-	num_2 = ll_to_num(node_2)
-	sum = num_1 + num_2
-	sum_str = str(sum)
-	head = None
-	for char in sum_str:
-		head = Node(char, head)
+	carry = 0
+	head = tail = None
+	while node_1 or node_2:
+		sum = carry
+		if node_1:
+			sum += node_1.value
+			node_1 = node_1.next
+		if node_2:
+			sum += node_2.value
+			node_2 = node_2.next
+
+		rem = sum % 10
+		carry = sum // 10  # gives how many times 10 fits into sum
+
+		if head:
+			tail.next = Node(rem)
+			tail = tail.next
+		else:
+			head = tail = Node(rem)
+
+	if carry: tail.next = Node(carry)
+
 	return head
 
-def ll_to_num(node):
-	res = []
-	while node != None:
-		res.append(str(node.value))
-		node = node.next
-	res.reverse() # reverses list in place (res[::-1] uses extra space)
-	return int(''.join(res))
-
 n6 = Node(1)
-n5 = Node(3, n6)
-n4 = Node(2, n5)
+n5 = Node(0, n6)
+n4 = Node(0, n5)
 
-n3 = Node(8)
-n2 = Node(3, n3)
-n1 = Node(4, n2)
+n2 = Node(9)
+n1 = Node(9, n2)
 
 res = []
 node = sum_lists(n1, n4)
