3a3dee3e3215955b156080d9df20266d5f0f576a
gutucristian <gutucristian27@gmail.com>
2018-05-06 10:02:30 -0400

    iterative approach

--- ch_2/2.6.py
+++ ch_2/2.6.py
@@ -24,10 +24,27 @@ def is_equal(n1, n2):
 		n2 = n2.next
 	return n1 == None and n2 == None
 
-n1_a = Node('a')
-n1_b = Node('b')
-n1_c = Node('a')
+# second possible solution
+def is_palindrome(head):
+	slow = fast = head
+	first_half = []
 
-n1_a.next, n1_b.next = n1_b, n1_c
+	while fast != None and fast.next != None:
+		first_half.append(slow.value)
+		slow = slow.next
+		fast = fast.next.next
+
+	# if linked list length is odd, then above while loop will stop bec. fast.next is None
+	if fast != None : slow = slow.next
+
+	while slow != None:
+		top = first_half.pop()
+		if top != slow.value:
+			return False
+		slow = slow.next
+	return True
+
+n1_a, n1_b, n1_c, n1_d = Node('a'), Node('b'), Node('b'), Node('a')
+n1_a.next, n1_b.next, n1_c.next = n1_b, n1_c, n1_d
 
 print(is_palindrome(n1_a))
