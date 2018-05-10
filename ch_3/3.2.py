76ab10d158bcee12b0444fb2183c63e111c8275f
gutucristian <gutucristian27@gmail.com>
2018-05-10 12:11:41 -0400

    make Stack parent class and reuse code

--- ch_3/3.2.py
+++ ch_3/3.2.py
@@ -1,59 +1,32 @@
-class StackNode:
+# 'import stack' will import entire stack.py module which also contains StackNode
+from stack import Stack
+from sys import maxsize
+
+class NodeWithMin:
   def __init__(self, value=None, next=None, min=None):
     self.value = value
     self.next = next
     self.min = min
 
-"""A stack is linear data structure that uses last in first out ordering (LIFO). Unlike arrays,
-stacks do not offer constant-time access to the ith item. However, it does allow constant-time adds
-and removes, as it does not require shifting elements around. The following is a stack implementation
-using linked lists.
-"""
-class Stack:
-  def __init__(self):
-    self.top = None
+class StackWithMin(Stack):
+  def min(self):
+    if self.is_empty():
+      return maxsize # error value
+    return self.peek(return_item=True)
 
-  # remove from front
-  def pop(self):
-    if self.top is None:
-      return None
-    value = self.top.value
-    self.top = self.top.next
-    return value
+  def push(self, value):
+    top = self.peek(return_item=True)
+    new_min = min(value, top.min) if top else value
+    super(StackWithMin, self).push(custom_node=NodeWithMin(value, self.top, new_min))
+    return self
 
-  # add to front
-  def push(self, value):           
-    if self.top:
-      cur_min = min(value, self.top.min)
-      t = StackNode(value, self.top, cur_min)            
-    else:  
-      t = StackNode(value, self.top, value)    
-    self.top = t
-    return self # return reference object reference to enable chaining
-  
-  def peek(self):
-    return self.top.value if self.top else None
-
-  def is_empty(self):
-    return self.top is None
-
-  def __iter__(self):
-    current = self.top
-    while current:
-      yield current
-      current = current.next
-
-  def __str__(self):
-    values = [str(node.value) for node in self]
-    return ' -> '.join(values)  
-
-stack = Stack()
+stack = StackWithMin()
 stack.push(4).push(2).push(1).push(9)
 print(stack)
-print(stack.top.min)
+print(stack.peek(return_item=True).min)
 stack.pop()
 print(stack)
 stack.pop()
 print(stack)
-print(stack.top.min)
+print(stack.peek(return_item=True).min)
 print(stack)
