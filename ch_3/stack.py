69babf595b9173896f48057ae47ab42777ac5da0
gutucristian <gutucristian27@gmail.com>
2018-05-10 12:16:02 -0400

    fix typo and rename optional parameter

--- ch_3/stack.py
+++ ch_3/stack.py
@@ -3,7 +3,7 @@ class StackNode:
     self.value = value
     self.next = next
 
-"""A stack is linear data structure that uses last in first out ordering (LIFO). Unlike arrays,
+"""A stack is a linear data structure that uses last in first out ordering (LIFO). Unlike arrays,
 stacks do not offer constant-time access to the ith item. However, it does allow constant-time adds
 and removes, as it does not require shifting elements around. The following is a stack implementation
 using linked lists.
@@ -35,13 +35,13 @@ class Stack:
       self.top = t
     return self # return reference object reference to enable chaining
   
-  def peek(self, return_item=False):
+  def peek(self, return_obj=False):
     """Returns value of the top item from the stack.
     
     Keyword arguments:    
-    return_item -- if True, returns item from top of stack
+    return_obj -- if True, returns object of top item from stack
     """
-    if return_item:
+    if return_obj:
       return self.top    
     return self.top.value if self.top else None
 
@@ -58,9 +58,9 @@ class Stack:
     values = [str(node.value) for node in self]
     return ' -> '.join(values)  
 
-# stack = Stack()
-# stack.push('a').push('b').push('c')
-# print(stack)
-# stack.pop()
-# stack.pop()
-# print(stack)
+stack = Stack()
+stack.push('a').push('b').push('c')
+print(stack)
+stack.pop()
+stack.pop()
+print(stack)
