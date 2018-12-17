501cf481b5b180414f7c53dac1074fe2c41bba1a
Cristian Gutu <gutucristian27@gmail.com>
2018-12-17 12:49:06 -0500

    Update 1.2.py

--- ch_1/1.2.py
+++ ch_1/1.2.py
@@ -19,4 +19,16 @@ def is_permutation_2(a, b):
 def is_permutation_3(a, b):
   return sorted(a) == sorted(b)
 
+def is_permutation4(str1, str2):
+  letters = [0] * 128 # assume characters are in regular ASCII character set
+  for char in str1:
+    letters[ord(char)] += 1
+
+  for char in str2:
+    letters[ord(char)] -= 1
+    if (letters[ord(char)] < 0):
+      return False
+
+  return True
+
 print(is_permutation_2('abc', 'aac'))
