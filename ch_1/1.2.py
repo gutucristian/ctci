581b83a24c92589a1a13e2055f5e77bf93786761
Cristian Gutu <gutucristian27@gmail.com>
2018-12-20 14:41:25 -0500

    Update 1.2.py

--- ch_1/1.2.py
+++ ch_1/1.2.py
@@ -4,31 +4,16 @@ from functools import reduce
 def is_permutation_1(a, b):
   return Counter(a) == Counter(b)
 
-def is_permutation_2(a, b):
-  if len(a) != len(b):
+def is_permutation(s1, s2):
+  if (len(s1) != len(s2)):
     return False
-  counter = Counter() # Counter is a dict subclass for counting hashable objects. It is an unordered collection where elements are stored as dictionary keys and their counts are stored as dictionary values.
-  for char in a:    
-    counter[char] += 1
-  for char in b:
-    if counter[char] == 0:
-      return False
-    counter[char] -= 1
-  return True
+     
+  content = [0] * 128
+  for char in s1:
+    content[ord(char)] += 1
+  for char in s2:
+    content[ord(char)] -= 1
+  return reduce(lambda x, y: x + y, content) == 0
 
 def is_permutation_3(a, b):
   return sorted(a) == sorted(b)
-
-def is_permutation4(str1, str2):
-  letters = [0] * 128 # assume characters are in regular ASCII character set
-  for char in str1:
-    letters[ord(char)] += 1
-
-  for char in str2:
-    letters[ord(char)] -= 1
-    if (letters[ord(char)] < 0):
-      return False
-
-  return True
-
-print(is_permutation_2('abc', 'aac'))
