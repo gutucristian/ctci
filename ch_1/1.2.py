9eeeaf0bfbf09c4c0827b2b738b953a55a91b495
Cristian Gutu <gutucristian27@gmail.com>
2018-12-20 16:45:05 -0500

    Update 1.2.py

--- ch_1/1.2.py
+++ ch_1/1.2.py
@@ -2,8 +2,12 @@ from collections import Counter
 from functools import reduce
 
 def is_permutation_1(a, b):
+  # time complexity is O(2n) = O(n) due to traversing a and b
+  # space complexity is O(2n) = O(n) for storing character frequencies of a and b in dictionaries
   return Counter(a) == Counter(b)
 
+# time complexity is O(n) due to traversal of s1 and s2
+# space complexity is O(1) since out content buffer always has length of 128
 def is_permutation(s1, s2):
   if (len(s1) != len(s2)):
     return False
@@ -15,5 +19,7 @@ def is_permutation(s1, s2):
     content[ord(char)] -= 1
   return reduce(lambda x, y: x + y, content) == 0
 
+# time complexity O(nlogn) due to sorting using Timsort
+# space complexity is O(n) because sortes() returns a new sorted list (it does not sort in place)
 def is_permutation_3(a, b):
   return sorted(a) == sorted(b)
