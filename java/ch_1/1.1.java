e7b2fdc1ae098c4225b98df0aa29f2bef73fa8cd
gutucristian <gutucristian27@gmail.com>
2019-06-08 14:22:21 -0400

    solve 1.2

--- java/ch_1/1.1.java
+++ java/ch_1/1.1.java
@@ -4,16 +4,38 @@ class Main {
     boolean isUnique = isUniqueChars(str);
     System.out.println(isUnique);
   }
-  
+
   static boolean isUniqueChars(String str) {
-    int checker = 0;
-    for (int i = 0; i < str.length(); ++i) {
-      int val = str.charAt(i) - 'a';
-      if ((checker & (1 << val)) > 0) return false;
-      checker |= (1 << val);
-    }
-    return true;
+      
   }
+
+  // time complexity O(n^2)
+  // space complexity O(1)
+  // static boolean isUniqueChars(String str) {
+  //   for (int i = 0; i < str.length() - 1; ++i) {
+  //     for (int j = i + 1; j < str.length(); ++j) {
+  //       // charAt() returns a char (which is a primitive type) at some index (i and j in this case)
+  //       // since chars are primitive types we can use == to compare for equality. For reference types (e.g., String)
+  //       // we must use the equals() method.
+  //       if (str.charAt(i) == str.charAt(j)) {
+  //         return false;
+  //       }
+  //     }
+  //   }
+  //   return true;
+  // }
+  
+  // time complexity O(1)
+  // space complexity O(1) (assumes a-z alphabet ONLY)
+  //static boolean isUniqueChars(String str) {
+  //  int checker = 0;
+  //  for (int i = 0; i < str.length(); ++i) {
+  //    int val = str.charAt(i) - 'a';
+  //    if ((checker & (1 << val)) > 0) return false;
+  //    checker |= (1 << val);
+  //  }
+  //  return true;
+  //}
 
   // time complexity O(1) (is we assume ASCII character set)
   // space complexity O(1) (we use a fixed size array)
