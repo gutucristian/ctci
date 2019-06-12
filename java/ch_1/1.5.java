c5000702c7bf4e5b1dac7e82f72ac4c4bec6a0ca
gutucristian <gutucristian27@gmail.com>
2019-06-12 09:16:46 -0400

    1.5 book solution

--- java/ch_1/1.5.java
+++ java/ch_1/1.5.java
@@ -4,23 +4,64 @@ class Main {
       System.out.println("usage: isOneEditAway a b");
     } else {
       System.out.printf("a=%s, b=%s\n", args[0], args[1]);
-      System.out.println(isOneEditAway(args[0], args[1], false));
+      // System.out.println(isOneEditAway(args[0], args[1], false));
+      System.out.println(isOneEditAway(args[0], args[1]));
     }
   }
 
-  static boolean isOneEditAway(String a, String b, boolean oneEdit) {
-    if (Math.abs(a.length() - b.length()) > 1) return false;
-    if (a.length() == 1 && b.length() == 1) {
-      if (oneEdit) return false;
-      if (a.equals(b)) return true;
-      return true;
+  static boolean isOneEditAway(String a, String b) {
+    if (a.length() == b.length()) {
+      return oneEditReplace(a, b);
+    } else if (a.length() + 1 == b.length()) {
+      return oneEditInsert(a, b);
+    } else if (a.length() - 1 == b.length()) {
+      return oneEditInsert(b, a);
     }
-    if (a.length() == 0 || b.length() == 0) {
-      System.out.printf("a = %s, b = %s, oneEdit = %b\n", a, b, oneEdit);
-      if (Math.abs(a.length() - b.length()) == 1 && !oneEdit) return true;
-      return false;
-    }
-    if (a.substring(0, 1).equals(b.substring(0, 1))) return isOneEditAway(a.substring(1), b.substring(1), oneEdit);
-    return isOneEditAway(a.substring(1), b, true) || isOneEditAway(a, b.substring(1), true);
+    return false;
   }
+
+  static boolean oneEditReplace(String a, String b) {
+    boolean oneEdit = false;
+    int i = 0, j = 0;
+    while (i < a.length()) {
+      if (a.charAt(i) != b.charAt(j)) {
+        if (oneEdit) return false;
+        oneEdit = true;
+      }
+      ++i;
+      ++j;
+    }
+    return true;
+  }
+
+  static boolean oneEditInsert(String a, String b) {
+    boolean oneInsert = false;
+    int i = 0, j = 0;
+    while (i < a.length()) {
+      if (a.charAt(i) != b.charAt(j)) {
+        if (oneInsert) return false;
+        oneInsert = true;
+        ++j;
+      } else {
+        ++i;
+        ++j;
+      }
+    }
+    return true;
+  }
+
+  // static boolean isOneEditAway(String a, String b, boolean oneEdit) {
+  //   if (Math.abs(a.length() - b.length()) > 1) return false;
+  //   if (a.length() == 1 && b.length() == 1) {
+  //     if (oneEdit) return false;
+  //     if (a.equals(b)) return true;
+  //     return true;
+  //   }
+  //   if (a.length() == 0 || b.length() == 0) {
+  //     if (Math.abs(a.length() - b.length()) == 1 && !oneEdit) return true;
+  //     return false;
+  //   }
+  //   if (a.substring(0, 1).equals(b.substring(0, 1))) return isOneEditAway(a.substring(1), b.substring(1), oneEdit);
+  //   return isOneEditAway(a.substring(1), b, true) || isOneEditAway(a, b.substring(1), true);
+  // }
 }
