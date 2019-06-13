5e0c7cfb21a78836de8866d457362e8bb8fbde2d
gutucristian <gutucristian27@gmail.com>
2019-06-13 15:28:10 -0400

    add StringBuilder solution

--- java/ch_1/1.6.java
+++ java/ch_1/1.6.java
@@ -7,18 +7,69 @@ class Main {
     }
   }
 
-  // time complexity O(p + k^2) where p = length of string and k = number of character sequences
-  // space complexity O(p)
-  static String compress(String str) {
-    String compressed = "";
-    int count = 0;
+  // time complexity O(p) where p = length of string
+  // space complexity O(1)
+  static int countCompression(String str) {
+    int compressedLength = 0;
+    int countConsecutive = 0;
     for (int i = 0; i < str.length(); ++i) {
-      ++count;
+      ++countConsecutive;
       if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
-        compressed += "" + str.charAt(i) + count;
-        count = 0;
+        compressedLength += 1 + String.valueOf(countConsecutive).length();
+        countConsecutive = 0;
       }
     }
-    return compressed.length() < str.length() ? compressed : str;
+    return compressedLength;
   }
+
+  // time complexity O(p + k) where p = length of string and k = number of elements in "compressed" StringBuilder object
+  // space complexity O(p)
+  static String compress(String str) {
+    int finalLength = countCompression(str);
+    if (finalLength >= str.length()) return str;
+    
+    int countConsecutive = 0;
+    StringBuilder compressed = new StringBuilder(finalLength);
+    for (int i = 0; i < str.length(); ++i) {
+      ++countConsecutive;
+      if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+        compressed.append(str.charAt(i));
+        compressed.append(countConsecutive);
+        countConsecutive = 0;
+      }
+    }
+
+    return compressed.toString();
+  }
+
+  // time complexity O(p + k) where p = length of string and k = number of character sequences
+  // space complexity O(p)
+  // static String compress(String str) {
+  //   StringBuilder compressed = new StringBuilder();
+  //   int count = 0;
+  //   for (int i = 0; i < str.length(); ++i) {
+  //     ++count;
+  //     if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+  //       compressed.append(str.charAt(i));
+  //       compressed.append(count);
+  //       count = 0;
+  //     }
+  //   }
+  //   return compressed.length() < str.length() ? compressed.toString() : str;
+  // }
+
+  // time complexity O(p + k^2) where p = length of string and k = number of character sequences
+  // space complexity O(p)
+  // static String compress(String str) {
+  //   String compressed = "";
+  //   int count = 0;
+  //   for (int i = 0; i < str.length(); ++i) {
+  //     ++count;
+  //     if (i + 1 >= str.length() || str.charAt(i) != str.charAt(i + 1)) {
+  //       compressed += "" + str.charAt(i) + count;
+  //       count = 0;
+  //     }
+  //   }
+  //   return compressed.length() < str.length() ? compressed : str;
+  // }
 }
