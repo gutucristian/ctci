d938203351a27506fb83adf6397249fa87eda4f1
Cristian Gutu <gutucristian27@gmail.com>
2019-07-24 20:55:25 -0400

    solve 1.8

--- python/ch_1/1.8.py
+++ python/ch_1/1.8.py
@@ -5,31 +5,39 @@ def set_zero(matrix):
   amt_of_rows = len(matrix)
   amt_of_cols = len(matrix[0])
 
+	# check if first row has a zero
   for col in range(amt_of_cols):
     if matrix[0][col] == 0:
       row_has_zero = True
       break
 
+	# check if first column has a zero
   for row in range(amt_of_rows):
     if matrix[row][0] == 0:
       col_has_zero = True
       break
 
+	# check for zeros in the rest of the array
   for row in range(amt_of_rows):
     for col in range(amt_of_cols):
       if matrix[row][col] == 0:
         matrix[0][col] = 0
         matrix[row][0] = 0
   
+	# zero out columns
   for col in range(1, amt_of_cols):
     if matrix[0][col] == 0:
       nullify_col(matrix, col)
 
+	# zero out rows
   for row in range(1, amt_of_rows):
     if matrix[row][0] == 0:
       nullify_row(matrix, row)
 
+	# zero our first row
   if row_has_zero: nullify_row(matrix, 0)
+
+	# zero out first column
   if col_has_zero: nullify_col(matrix, 0)
 
   return matrix
